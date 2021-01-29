fb43b160943834cc851b427f821913692e5c1dde
Moses Ramirez <mosesgramirez@gmail.com>
2021-01-29 16:17:42 -0600

    Add stack and drawer navigation to components

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
 // Note: require() is a node.js function.
 // Note: onPress() is a method of ListItem.
+// Each 'screen' component is automatically provided with the 'navigation' prop.
 
-export default function Directory(props) {
-    
-    const renderDirectoryItem = ({item}) => {
+
+class Directory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+
+    // Configure text for header. "static" is a js keyword.
+    static navigationOptions = {
+        title: 'Directory'
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+        const renderDirectoryItem = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                    leftAvatar={{ source: require('./images/react-lake.jpg')}}
+                />
+            );
+        };
+
         return (
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                onPress={() => props.uponPress(item.id)}
-                leftAvatar={{ source: require('./images/react-lake.jpg')}}
-             />   
+            <FlatList
+                data={this.state.campsites}
+                renderItem={renderDirectoryItem}
+                keyExtractor={item => item.id.toString()}
+            />
         );
-    };
-
-    return (
-        <FlatList
-            data={props.campsites}
-            renderItem={renderDirectoryItem}
-            keyExtractor={item => item.id.toString()}
-        />
-    );
+    }
 }
+
+export default Directory;
