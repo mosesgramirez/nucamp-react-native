466c9effb75849182a589c139356c8b17139384c
Moses Ramirez <mosesgramirez@gmail.com>
2021-02-19 16:59:26 -0600

    Add user registration component

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://192.168.0.114:3001/';
+export const baseUrl = 'http://192.168.0.8:3001/';
 // export const baseUrl = 'https://aaronmichael.herokuapp.com/nucamp-data/';
